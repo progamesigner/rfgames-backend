ebacff5c60ce2cfbe71b35c327cddfbaf944a9c9
Yang Sheng Han <progamesigner@gmail.com>
2019-12-14 13:30:11 +0800

    Publish container image to Docker Hub

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 docker build \
     --tag docker.pkg.github.com/progamesigner/rfgames-backend/server:1.0 \
+    --tag progamesigner/rfgames-backend-server:1.0 \
     .
 
 docker push docker.pkg.github.com/progamesigner/rfgames-backend/server:1.0
+
+docker push progamesigner/rfgames-backend-server:1.0
